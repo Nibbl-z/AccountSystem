2e146c9cc4447d0919290371aeda2092fc6fb5ca
Nibbl-z <thisisnotnibblz123@gmail.com>
2024-12-20 07:57:51 -0700

    lllllllearning the ropes !

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Signup from './pages/Signup.tsx'
 
 function App() {
   return (
-    <>
-      <h1>Hai!</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Signup/>}/>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ client/src/pages/Signup.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../styles/Signup.css'
+
+function Signup() {
+    return (
+        <div class='wrapper'>
+            <div class='container'>
+                <h1>hiiii</h1>
+            </div>
+        </div>
+    )  
+}
+
+export default Signup
